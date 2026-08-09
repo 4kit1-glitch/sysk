d99b2f2d9b8d0bf74935312a1236fa4869628b3a
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-09 16:59:42 +0100

    feat(devices): add script for termal info

---
+++ lib/devices/sys/thermal.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# vim: noai:ts=4:sw=4:expandtab
+# shellcheck source=/dev/null
+# shellcheck disable=2086,2155,2034
+# fan speeds /sys/class/hwmon/fan*_input
+# temparature
+# - disk 
+# - cpu and gpu
+# - general system temparature
+
+
+readonly SYSTEM_THERMAL_ZONE_FILE="/sys/class/thermal/thermal_zone*"
+readonly SYSTEM_THERMAL_ZONES=$(find /sys/class/thermal/thermal_zone* | wc -l)
+
+
+get_avg_temp(){
+    # general system temparature, mother board and 
+    # average_temp = average temparature in degree celcius 
+    # total_millic combined system temparature from all thermal zones
+    
+    
+    local total_millic=$(paste $SYSTEM_THERMAL_ZONE_FILE/temp 2> /dev/null | \
+        awk 'BEGIN{sum=0}{for(i=1; i<=NF; i++)sum+=$i} END {print sum}')
+
+    local average_temp=$(bc -q <<< "scale=2; ($total_millic / $SYSTEM_THERMAL_ZONES) / 1000")
+    echo "$total_millic"
+}
+get_avg_temp
