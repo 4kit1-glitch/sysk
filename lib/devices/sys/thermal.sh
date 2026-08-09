3b5e4b8b029dbc7a5a69841083ef3fbdf4f0b639
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-09 17:38:23 +0100

    feat(thermal): add functions to get system thermal information

--- lib/devices/sys/thermal.sh
+++ lib/devices/sys/thermal.sh
@@ -9,18 +9,40 @@
 # - general system temparature
 
 
-readonly SYSTEM_THERMAL_ZONE_FILE="/sys/class/thermal/thermal_zone*"
-readonly SYSTEM_THERMAL_ZONES=$(find /sys/class/thermal/thermal_zone* | wc -l)
+readonly SYSTEM_THERMAL_ZONE_PATH="/sys/class/thermal/thermal_zone"
+readonly SYSTEM_THERMAL_ZONES=$(find $SYSTEM_THERMAL_ZONE_PATH* | wc -l)
 
 
-get_avg_temp(){
-    # general system temparature, mother board and 
-    # average_temp = average temparature in degree celcius 
-    # total_millic combined system temparature from all thermal zones
-    local total_millic=$(paste $SYSTEM_THERMAL_ZONE_FILE/temp 2> /dev/null | \
-        awk 'BEGIN{sum=0}{for(i=1; i<=NF; i++)sum+=$i} END {print sum / (i * 1000)}')
-    local average_temp=$(bc -q <<< "scale=2; $total_millic / 1000")
-    echo $total_millic
-    printf "%.2f*C" "$average_temp"
+
+get_avg_temp() {
+    # general system temparature
+    awk '
+    BEGIN { sum = 0; count=0; max=0}
+    {   
+        sum += $1
+        count++
+        if ($1 > max) max = $1    
+    }
+    END {
+        if (count == 0) {printf "N/A"; exit 1}
+        avg = sum / (count * 1000)
+
+        printf "%.2f*c", avg
+    }' "$SYSTEM_THERMAL_ZONE_PATH"*/temp 2> /dev/null
 }
-get_avg_temp
+get_zone_temps() {
+
+}
+get_cpu_temp() {
+
+}
+
+get_gpu_temp() {
+
+}
+
+get_fan_status() {
+    # number of fans
+    # speed and status
+
+}
