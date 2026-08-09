18ba83d72dac510f00e11b42d136a243fb68ae31
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-09 19:04:08 +0100

    feat: add celcius converter function

--- lib/devices/sys/thermal.sh
+++ lib/devices/sys/thermal.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vim: noai:ts=4:sw=4:expandtab
 # shellcheck source=/dev/null
-# shellcheck disable=2086,2155,2034
+# shellcheck disable=2086,2155
 # fan speeds /sys/class/hwmon/fan*_input
 # temparature
 # - disk 
@@ -10,39 +10,35 @@
 
 
 readonly SYSTEM_THERMAL_ZONE_PATH="/sys/class/thermal/thermal_zone"
-readonly SYSTEM_THERMAL_ZONES=$(find $SYSTEM_THERMAL_ZONE_PATH* | wc -l)
+readonly SYSTEM_THERMAL_ZONES=$(find "$SYSTEM_THERMAL_ZONE_PATH"*/type | wc -l)
 
+convert_to_celcius() {
+    # converts temp in millicelcius to celcius
+    local -r value_tc=$1 # value to convert
+    celcius=$( bc -q <<< "scale=3; $value_tc / 1000" )
 
-
+    printf "%.2f*C" "$celcius"
+}
 get_avg_temp() {
     # general system temparature
-    awk '
-    BEGIN { sum = 0; count=0; max=0}
-    {   
-        sum += $1
-        count++
-        if ($1 > max) max = $1    
-    }
-    END {
-        if (count == 0) {printf "N/A"; exit 1}
-        avg = sum / (count * 1000)
-
-        printf "%.2f*c", avg
-    }' "$SYSTEM_THERMAL_ZONE_PATH"*/temp 2> /dev/null
+    echo pass
 }
 get_zone_temps() {
-
+    for zone in "$SYSTEM_THERMAL_ZONE_PATH"*; do 
+        echo "$(cat "$zone"/type)=\"$(cat $zone/temp  2> /dev/null || { printf "N/A"; } | )\""
+    done
 }
 get_cpu_temp() {
-
+    echo pa
 }
 
 get_gpu_temp() {
-
+echo pass
 }
 
 get_fan_status() {
     # number of fans
     # speed and status
-
+    echo pass
 }
+get_zone_temps
