54775c26c7914aab086fec5c6d5f7b434b66874d
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-13 20:32:13 +0100

    feat: add anon page info

--- lib/devices/storage/memory.sh
+++ lib/devices/storage/memory.sh
@@ -107,7 +107,7 @@ get_free_virtual() {
     echo pass
 }
 
-#-----------------------------
+#----------OTHERS-------------------
 get_hardware_corrupted() {
     # reads bad ram pages
     read_meminfo "HardwareCurrupted"
@@ -121,100 +121,13 @@ get_unevictable_mem() {
 get_balloon_mem() {
     # gets Ballon memory thats used by Virtual machines
     read_meminfo "Balloon"
-
 }
 
 get_dirty_mem() {
+    # gets memory waiting to write to disk
     read_meminfo "Dirty"
 }
-# cpu level memory
-get_cpu_cache_info() {
-    echo pa
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# quick details about ram 
-get_short_ram_info() {
-    local short_ram_info="$(
-        free -h | sed -n '/Mem/p' | 
-        awk '{ printf "total: %10s\nused: %11s\navailable: %6s", $2, $3, $7 end}' | 
-        sed 's/Gi/Gb/'
-    )"
-    
-    printf "%s\n" "$short_ram_info"
-}
-
-# full details about ram
-get_full_ram_info() {
-    run_privileged dmidecode -t memory | sed -E -n -f "$FEATURE_DIR/full_ram.sed" |
-    awk 'BEGIN {PARAMETERS=8; printf "memory devices found \n"} NR % PARAMETERS == 1 {print "----- Device" ++n " -----"} { print }'
-
-    return $?
-}
-
-#------------------ CACHE-------------------------
-
-# quick info on the cache
-get_short_cache_info() {
-    short_cache_info="$(
-        free -h | grep -Ei "Mem" | awk '{ printf "cache memory: %s\n", $6 }' | sed  s/Gi/Gb/
-    )"
-    printf "%s\n" "$short_cache_info"
-}
-
-# deep cpu cache details
-get_ext_cache_info() {
-
-    printf "%s\ncpu cache info\n%s\n" "$(get_short_cache_info)" \
-    "$(lscpu | sed -E -n '/^L[[:digit:]]i?d?/p')"
-    return $?
-}
-
-# ---------------- processes --------------------------
-generate_memory_summary() {
-    local -ri TOTAL_MEM_DEVICES="$(
-        run_privileged dmidecode -t memory | 
-        grep -Ei "Number of devices" | awk '{ print $4}'
-    )"
-
-    printf "Total number of memory devices %d\n" "$TOTAL_MEM_DEVICES"
-    get_short_ram_info
-    get_short_cache_info
-    
-    return $?
+get_anon_pages() {
+    # reads anonpage size
+    read_meminfo "AnonPages"
 }
