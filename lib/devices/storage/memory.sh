652b768ce9486d2cbdbd3d1242b825b63b855b69
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-13 19:27:42 +0100

    feat: add more functions to get memory info

--- lib/devices/storage/memory.sh
+++ lib/devices/storage/memory.sh
@@ -38,50 +38,78 @@ convert() {
     printf "%s Mb" "$result"
 }
 
+
 read_meminfo() {
+    # reads and extract appropraite data from /proc/meminfo
     local -r FIELD="$1"
     convert "$(cat $MEM_INFO_FILE | awk -v field="$FIELD" '$0 ~ "^"field {print $2}')" "KB"
 }
 
+
+#---------- RAM ---------------
 get_total_mem() {
+    # get total memory
     read_meminfo "MemTotal"
 }
 get_available_mem() {
+    # get available memory
     read_meminfo "MemAvailable"
 }
 get_used_mem() {
+    # gets used memory
     echo pass
 }
+
+# ------ CACHE / BUFFERS -----------------
 get_cached_mem() {
+    # get cached memory
     read_meminfo "Cached"
 }
-get_swap_mem() {
-    echo pa
+get_buffered_mem() {
+    read_meminfo "Buffers"
+}
+get_swap_cache() {
+    # gets double buffered swap
+    read_meminfo "SwapCached"
 }
 get_used_cached() {
+    # calculates and echos used cached
     echo pa
 }
+# --------- SWAP-------------
+get_swap_mem() {
+    # gets swap memory
+    read_meminfo SwapTotal
+}
+get_free_swap() {
+    # gets unused swap
+    read_meminfo SwapFree
+}
+
 get_used_swap() {
-    echo pa
-}
-get_used_cache() { 
+    # calculates used swap
     echo pa
 }
 
+# -------- VIRTUAL MEMORY -----------
+get_virtual_mem() {
+    # gets total virtual memory
+    read_meminfo "VmallocTotal"
+}
+
+get_used_virtual() {
+    # gets uded virtual memory
+    read_meminfo "VmallocUsed"
+}
+
+get_free_virtual() {
+    # gets ramaining virtual mem
+    echo pass
+}
 # cpu level memory
 get_cpu_cache_info() {
     echo pa
 }
-
-get_memory_device_info() {
-    echo pa
-}
-
-get_total_mem
-get_cached_mem
-
-
-
 
 
 
