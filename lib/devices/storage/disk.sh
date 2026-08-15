6eaaf11996f7fd3bd00205e740790c96126db8a1
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-15 12:30:32 +0100

    feat(dislk): add disk check and error message

--- lib/devices/storage/disk.sh
+++ lib/devices/storage/disk.sh
@@ -3,9 +3,6 @@
 #
 # this performs disk checks and on disk drives and stores key info in disk.conf
 
-
-mapfile -t drives < <(run_smartctl --scan | cut -d' ' -f 1)
-
 run_smartctl() {
     # specifies and runs smartctl
     local flag="$1"
@@ -14,12 +11,17 @@ run_smartctl() {
     run_privileged smartctl "$flag" "$drive"
 }
 
+mapfile -t drives < <(run_smartctl --scan | cut -d' ' -f 1) # store disk locations in a drives 
+
 scan_disks() {
+    [[ ${#drives[@]} -eq 0 ]] && {
+        printf "no disk found!!" >&2
+        return 1
+    }
     for drive in "${drives[@]}"; do
         printf "%s\n" "$drive"
+    done
 }
-echo "${drives[*]}"
-
 get_disk_num() {
     scan_disks | wc -l
 }
