7739aa50a5c2d9dfe5ff1f333cfa86a44dd630e7
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-16 12:06:30 +0100

    docs: add documentation on error codes meaning

--- lib/core/00_error.sh
+++ lib/core/00_error.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # shellcheck source=/dev/null
 # shellcheck disable=2034
-
+#
 # script defines and stored error codes used in the program
 # code convension: ERR_WHAT_IT_REPRESENTS
-# make it as short as possible    fi
+# make it as short as possible
 
 readonly ERR_SUCCESS=0  # operation successfule
 readonly ERR_FAILURE=1  # operation failed general 
 readonly ERR_PERMISSION_DENIED=126 # permission denied
 readonly ERR_COMMAND_FAILED=126 # command runs but fails
 readonly ERR_NOT_FOUND=127  # command or file not found
-readonly ERR_BAD_USAGE=2    # bad use of script
+readonly ERR_BAD_USAGE=2    # bad use of script or function
 
 
 
