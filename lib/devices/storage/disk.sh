1ea82f734967db93f9950845d459b84e0e0e9e79
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-17 19:29:40 +0100

    feat: add get_drive function

--- lib/devices/storage/disk.sh
+++ lib/devices/storage/disk.sh
@@ -10,3 +10,51 @@
 readonly DRIVE_DIR="/sys/block"
 readonly DISK_STATS_FILE="/proc/diskstats"
 
+mapfile -t drives < <(find "$DRIVE_DIR"/* -maxdepth 1 -printf "%f\n" | grep -Ev "(loops|ram|zram)") 
+
+
+# to read
+convert_to_gb() {
+    # converts
+    echo pass
+
+}
+
+disk_type_check() {
+    # tests disk type
+    echo pass
+
+}
+
+get_drives() {
+    # reads and prints all drives
+    for drive in ${drives[@]}; do 
+        printf "%s\n" "$drive"
+    done
+}
+
+get_drive_size() {
+    # prints the size of a specific drive
+}
+
+get_used_drive_space() {
+    # get already used space
+}
+
+get_free_drive_space() {
+
+}
+
+get_usb_drives() {
+    # prints usb drives
+
+}
+
+get_drive_partitions() {
+    # prints partitions for every drive
+}
+
+write_disk_json() {
+    # creates disk.json 
+
+}
