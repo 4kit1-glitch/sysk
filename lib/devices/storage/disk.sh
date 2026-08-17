5d50f404bc44ede2a22f0b864100997e29e295ea
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-17 19:47:01 +0100

    feat: add byte sector unit converter

--- lib/devices/storage/disk.sh
+++ lib/devices/storage/disk.sh
@@ -15,9 +15,16 @@ mapfile -t drives < <(find "$DRIVE_DIR"/* -maxdepth 1 -printf "%f\n" | grep -Ev 
 
 # to read
 convert_to_gb() {
-    # converts
-    echo pass
+    # converts VALUES in 512 - byte sectors to gb
+    local -r MULTIPLYER=512
+    local -r DIVIDER=1073741824
+    local -r VALUE="$1"
+    local value_b=$(( VALUE * MULTIPLYER ))
 
+    printf "%.1fGib" "$(bc -q <<< "scale=1; $value_b / $DIVIDER")" || {
+        echo "function failed" 2> /dev/null
+        return "$ERR_BAD_USAGE"
+    }
 }
 
 disk_type_check() {
@@ -28,33 +35,36 @@ disk_type_check() {
 
 get_drives() {
     # reads and prints all drives
-    for drive in ${drives[@]}; do 
+    for drive in "${drives[@]}"; do 
         printf "%s\n" "$drive"
     done
 }
 
 get_drive_size() {
     # prints the size of a specific drive
+    echo pass
 }
 
 get_used_drive_space() {
     # get already used space
+    echo pass
 }
 
 get_free_drive_space() {
-
+    echo pass
 }
 
 get_usb_drives() {
     # prints usb drives
-
+    echo pass
 }
 
 get_drive_partitions() {
     # prints partitions for every drive
+    echo pass
 }
 
 write_disk_json() {
     # creates disk.json 
-
+    echo pass
 }
