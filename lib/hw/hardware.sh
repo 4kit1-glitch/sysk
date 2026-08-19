1e84b121ef0871c78d6f356d5986d0f87ecf48e2
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-19 23:03:13 +0100

    feat(hardware): add dmi decode wrapper functions

--- lib/hw/hardware.sh
+++ lib/hw/hardware.sh
@@ -12,15 +12,78 @@
 # memory
 # disks
 
+
+readonly DMI_CACHE="$DUMPS_PATH/dmi_$DATE.cache"
+
+init_dmi_cache() {
+    # the info gotten is kinda static meaning tracking with date deosn't is not proper 
+    [[ ! -f "$DMI_CACHE" ]] && run_privileged dmidecode -t 0,1,2,4,16,17,22,39 > "$DMI_CACHE" || {
+        printf "cache already initialized...\n" >&2
+        return "$ERR_FAILURE"
+    }
+}
 read_dmi() {
-    # function is a wrapper to dmidecode
     local -r TYPE="$1"
     local -r FIELD="$2"
 
-    run_privileged dmidecode -q -t "$TYPE" | awk -F': ' -v field="$FIELD" '$0 ~ "^[[:space:]]*"field {print $2}'
+    cat "$DMI_CACHE" | sed -n "/type $TYPE,/,/^$/p" | \
+    awk -F': ' -v field="$FIELD" '$0 ~ "^[[:space:]]*"field {print $2}'
 }
 
-#---------------RAM-----------------------------
+
+#----- platform firmware-----
+
+get_vendor(){
+    read_dmi 0 "Vendor" || {
+        printf "N/A" >&2
+        return "$ERR_SUCCESS"
+    }
+}
+
+get_firmware_version() {
+    read_dmi 0 "Version"
+}
+
+get_release_date() {
+    read_dmi 0 "Release Date"
+}
+
+
+# --- system information-----
+get_manufacturer() {
+    read_dmi 1 "Manufacturer"
+}
+
+get_product_name() {
+    read_dmi 1 "Product"
+}
+get_system_version() {
+    read_dmi 1 "Version"
+}
+
+get_serial_number() {
+    read_dmi 1 "Serial"
+}
+
+# ------- processor info -----------
+get_processorr_type() {
+    read_dmi 4 "Type"
+}
+
+get_processor_family() {
+    read_dmi 4 "Family"
+}
+get_processor_version() {
+    read_dmi 4 "Version"
+}
+get_max_speed() {
+    read_dmi 4
+}
+get_current_speed() {
+    read_dmi 4
+}
+
+#---------------physical memory space-----------------------------
 # processing dmidecode
 get_installed_mem_num() { 
     read_dmi 16 "Number" 
@@ -28,6 +91,3 @@ get_installed_mem_num() {
 get_maximum_capacity() {
     read_dmi 16 "Maximum"
 }
-
-
-
