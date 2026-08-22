d656c117719e4bd624a3cae2a4275144bdb4cb26
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-22 12:11:39 +0100

    docs: add intro comment on the inference script

--- engine/inference.py
+++ engine/inference.py
@@ -1,1 +1,4 @@
 # inference engine for sysk adn sysd
+# I decided to use a categorical approach for inferencing 
+# rather than a numerical one for accuracy reasons
+# rules are found in engine/.rules/*.yaml
