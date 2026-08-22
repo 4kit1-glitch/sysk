04b8d73fef949eb040e1986753b5fbe1286d816a
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-23 00:17:50 +0100

    feat(engine): add modules for importing

--- engine/inference.py
+++ engine/inference.py
@@ -2,3 +2,13 @@
 # I decided to use a categorical approach for inferencing 
 # rather than a numerical one for accuracy reasons
 # rules are found in engine/.rules/*.yaml
+
+from sys import exit, argv
+from os import environ, path, makedirs
+from json import load, dump
+from yaml import safe_load
+
+DATE = environ.get("DATE")
+PROG_PATH = environ.get("SCRIPT_DIR")   # program path
+
+
